fa48335d28502b911af2960cefd7fd928fbcc7e3
Rohangajeelee <83898021+Rohangajeelee@users.noreply.github.com>
2022-07-27 21:13:52 +0530

    Add files via upload

---
+++ Sos/src/main/java/com/safety/wsafety/activity_Developer.java
@@ -0,0 +1,14 @@
+package com.safety.wsafety;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_Developer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer);
+    }
+}
